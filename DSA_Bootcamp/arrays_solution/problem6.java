c1c5a3ecd695aff31eedc13f2869de29a422c47a
alokverma <alokverma749@gmail.com>
2021-12-02 23:13:26 +0530

    added some more solutions

---
+++ DSA_Bootcamp/arrays_solution/problem6.java
@@ -0,0 +1,11 @@
+package DSA_Bootcamp.arrays_solution;
+
+import java.util.List;
+
+public class problem6 {
+    public static void main(String[] args) {
+
+    }
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    }
+}
